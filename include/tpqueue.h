d4c6f586b5a8a545f925756afc5b4ef82a1a762e
xakerbux <156354805+xakerbux@users.noreply.github.com>
2026-06-07 23:44:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,69 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template<typename T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& val) : data(val), next(nullptr) {}
+    };
+
+    Node* head;
+
+ public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(const T& item) {
+        Node* newNode = new Node(item);
+        if (isEmpty() || item.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while (current->next != nullptr &&
+               current->next->data.prior >= item.prior) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            return T();
+        }
+        Node* temp = head;
+        T result = head->data;
+        head = head->next;
+        delete temp;
+        return result;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    T get() const {
+        if (isEmpty()) {
+            return T();
+        }
+        return head->data;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
